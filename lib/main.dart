80149e4be238680fb8d982ee7606a2ad29edb601
Nadin Abduljabbar <ndn@Nadeens-MacBook-Pro.local>
2026-04-14 23:17:14 +0300

    Update latest changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'admin/AdminBinManagment.dart';
-void main() {
-=======
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'SplashScreen.dart';
@@ -14,7 +10,6 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
->>>>>>> 6f86ec77353f629cb07cc961dd574a81b42babf0
   runApp(const ReLeafApp());
 }
 
@@ -26,7 +21,7 @@ class ReLeafApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ReLeaf',
-      home: const AdminBinManagment(),
+      home: const SplashScreen(),
     );
   }
 }
