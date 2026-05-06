7099c61a3b6c4291b62af74dac125f54c75e9b5b
Fatemah <fatimabarri722@gmail.com>
2026-05-06 21:57:56 +0300

    Restore my version

--- lib/user/llm/Chatbot.dart
+++ lib/user/llm/Chatbot.dart
@@ -18,7 +18,7 @@ class _ChatbotState extends State<Chatbot> {
 
   bool _isLoading = false;
 
-  final String _baseUrl = 'http://172.20.10.2:3000';
+  final String _baseUrl = 'http://localhost:3000';
 
   final List<String> _quickQuestions = [
     'Where do I throw a plastic bottle?',
